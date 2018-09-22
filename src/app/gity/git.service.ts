fa0afe12e3911d919f0b62ddbd8ba432db726e1b
Wess58 <wesleymbate@gmail.com>
2018-09-22 16:07:17 +0300

    declared API keys

--- src/app/gity/git.service.ts
+++ src/app/gity/git.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitService {
+  private username: string;
+  private clientID: 'b0096ac6d420a3b2c4ce';
+  private clientSecret:'5fd881e86b8fe14975c7a703a825f8c95ee67bab';
 
-  constructor() { }
+  constructor(private http: Http) {
+    this.username = 'Wess58';
+  }
+
+
 }
