2351d7bf07bbcef545c0b44f1053e7d41822ff6b
Wess58 <wesleymbate@gmail.com>
2018-09-22 16:04:50 +0300

    created router constructor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule, Headers } from '@angular/http';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { RepoComponent } from './repo/repo.component';
+import { GitService } from './gity/git.service';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RoutingModule } from './routing/routing.module';
+import { PriceDirective } from './price.directive';
+import { PropPipe } from './prop.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    RepoComponent,
+    NotFoundComponent,
+    PriceDirective,
+    PropPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RoutingModule
   ],
-  providers: [],
+  providers: [GitService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
