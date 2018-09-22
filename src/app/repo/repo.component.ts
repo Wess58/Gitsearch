d661afd0f474fabed9fafdccff97ce88f280ab7c
Wess58 <wesleymbate@gmail.com>
2018-09-22 16:16:56 +0300

    created repo component

---
+++ src/app/repo/repo.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GitService } from '../gity/git.service';
+
+@Component({
+  selector: 'app-repo',
+  templateUrl: './repo.component.html',
+  styleUrls: ['./repo.component.css']
+})
+export class RepoComponent implements OnInit {
+
+  
+
+  ngOnInit() {
+  }
+
+}
