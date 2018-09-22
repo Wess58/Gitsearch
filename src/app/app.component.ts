c4c7a85bab6b9961b8e409a1d0142880f051aad9
Wess58 <wesleymbate@gmail.com>
2018-09-22 16:05:54 +0300

    created router constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Gitsearch';
+  router:Router;
+  constructor(route:Router){
+    this.router=route;
+
+  }
 }
