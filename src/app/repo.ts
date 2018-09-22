c3abbe4ab5526476e15b983b93e68b270ad494de
Wess58 <wesleymbate@gmail.com>
2018-09-22 16:20:31 +0300

    created class

--- src/app/repo.ts
+++ src/app/repo.ts
@@ -1,2 +1,3 @@
 export class Repo {
+  // constructor(public id: number, public name: string, public description: string, public homepage: string, public forks: number, public fork: boolean, public notLive: boolean)
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,4 @@
 export class User {
+  // constructor(
+  //   public login: string, public url: string, public repos: string, public followers: string, public following: string)
 }
