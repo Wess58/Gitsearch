0274b479152089323f1b73a45d499c224aba62a3
Wess58 <wesleymbate@gmail.com>
2018-09-22 17:12:02 +0300

    put APIkeys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '9bf6a6b77c4faad0d1d6b1f1bc562d12a7b3f664',
+  clientID: 'b0096ac6d420a3b2c4ce',
+  clientSecret:'5fd881e86b8fe14975c7a703a825f8c95ee67bab'
 };
 
 /*
