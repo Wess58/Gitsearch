323d22e9779cd732551a171439aa1a646508329e
Wess58 <wesleymbate@gmail.com>
2018-09-24 10:37:30 +0300

    used environment to abstract APIkeys

--- src/app/gity/git.service.ts
+++ src/app/gity/git.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { map } from 'rxjs/operators';
-
+import {environment} from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class GitService {
   private username: string;
-  private clientID: 'b0096ac6d420a3b2c4ce';
-  private clientSecret:'5fd881e86b8fe14975c7a703a825f8c95ee67bab';
 
   constructor(private http: Http) {
     this.username = 'Wess58';
@@ -16,11 +14,11 @@ export class GitService {
 
 
   getProfileInfo() {
-    return this.http.get("https://api.github.com/users/" + this.username + '?client_id=' + this.clientID + '?client_secret=' + this.clientSecret).pipe(map(res => res.json()));
+    return this.http.get("https://api.github.com/users/" + this.username + '?client_id=' + environment.clientID + '?client_secret=' + environment.clientSecret).pipe(map(res => res.json()));
   }
 
   getRepo() {
-    return this.http.get("https://api.github.com/users/" + this.username + '/repos?client_id=' + this.clientID + '?client_secret=' + this.clientSecret).pipe(map(res => res.json()));
+    return this.http.get("https://api.github.com/users/" + this.username + '/repos?client_id=' + environment.clientID + '?client_secret=' + environment.clientSecret).pipe(map(res => res.json()));
   }
 
   updateProfile(username: string) {
