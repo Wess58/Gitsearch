72d0c488220cac856965062bac4aed9e9685de57
wesley mbate <wmbate@meliora.tech>
2021-01-07 15:49:30 +0300

    removed env

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-// import { environment as common } from './environment';
-
-export const environment = {
-  production: false,
-  clientID: 'b0096ac6d420a3b2c4ce',
-  clientSecret: '5fd881e86b8fe14975c7a703a825f8c95ee67bab',
-  hubTkn: '9804a2e9d2852f5848f11567d505d2598d9622d0'
-};
