480c68fd9d0a50fcef538e1d28373c30d0e36b5e
wesley mbate <wmbate@meliora.tech>
2021-01-07 15:29:41 +0300

    doc deployment

--- src/app/gity/git.service.ts
+++ src/app/gity/git.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { map } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 @Injectable({
   providedIn: 'root'
 })
@@ -15,17 +15,17 @@ export class GitService {
 
 
   getProfileInfo(name: string) {
-    return this.http.get("https://api.github.com/users/" + name + '?access_token=' + environment.accessToken).pipe(map(res => res.json()));
+    return this.http.get("https://api.github.com/users/" + name + '?access_token=' + environment.hubTkn).pipe(map(res => res.json()));
   }
 
   getUserRepos(name: string) {
-    return this.http.get("https://api.github.com/users/" + name + '/repos?access_token=' + environment.accessToken).pipe(map(res => res.json()));
+    return this.http.get("https://api.github.com/users/" + name + '/repos?access_token=' + environment.hubTkn).pipe(map(res => res.json()));
 
     // return this.http.get("https://api.github.com/users/" + name + '/repos?client_id=' + environment.clientID + '?client_secret=' + environment.clientSecret).pipe(map(res => res.json()));
   }
 
   getContributors(name:string ,repo:string){
-    return this.http.get("https://api.github.com/repos/" + name +"/"+ repo +"/contributors" + '?access_token=' + environment.accessToken).pipe(map(res => res.json()));
+    return this.http.get("https://api.github.com/repos/" + name +"/"+ repo +"/contributors" + '?access_token=' + environment.hubTkn).pipe(map(res => res.json()));
 
   }
 }
