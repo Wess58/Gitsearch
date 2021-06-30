d7eab4776542a6772c9783411ef756f63487fce7
wesley mbate <wmbate@meliora.tech>
2021-06-30 12:31:05 +0300

    ready for deployment

--- src/app/gity/git.service.ts
+++ src/app/gity/git.service.ts
@@ -11,7 +11,8 @@ export class GitService {
   token: string;
   endPart:any = '2czyM0kXjFJ';
   // new token
-  // ghp_embRIRr39sZBpk5GbQsfjIcVr 2czyM0kXjFJ
+  // ghp_embRIRr39sZBpk5GbQsfjIcVr
+  //  2czyM0kXjFJ
 
 
 
@@ -25,19 +26,19 @@ export class GitService {
   getProfileInfo(name: string) {
     // console.log(token);
 
-    return this.http.get("https://api.github.com/users/" + name + '?access_token=' + 'ghp_embRIRr39sZBpk5GbQsfjIcVr' + this.endPart.toString()).pipe(map(res => res.json()));
+    return this.http.get("https://api.github.com/users/" + name + '?access_token=' + 'ghp_embRIRr39sZBpk5GbQsfjIcVr' + this.endPart).pipe(map(res => res.json()));
   }
 
   getUserRepos(name: string) {
 
-    return this.http.get("https://api.github.com/users/" + name + '/repos?access_token=' + 'ghp_embRIRr39sZBpk5GbQsfjIcVr' + this.endPart.toString()).pipe(map(res => res.json()));
+    return this.http.get("https://api.github.com/users/" + name + '/repos?access_token=' + 'ghp_embRIRr39sZBpk5GbQsfjIcVr' + this.endPart).pipe(map(res => res.json()));
 
     // return this.http.get("https://api.github.com/users/" + name + '/repos?client_id=' + environment.clientID + '?client_secret=' + environment.clientSecret).pipe(map(res => res.json()));
   }
 
   getContributors(name: string, repo: string) {
 
-    return this.http.get("https://api.github.com/repos/" + name + "/" + repo + "/contributors" + '?access_token=' + 'ghp_embRIRr39sZBpk5GbQsfjIcVr' + this.endPart.toString()).pipe(map(res => res.json()));
+    return this.http.get("https://api.github.com/repos/" + name + "/" + repo + "/contributors" + '?access_token=' + 'ghp_embRIRr39sZBpk5GbQsfjIcVr' + this.endPart).pipe(map(res => res.json()));
 
   }
 }
